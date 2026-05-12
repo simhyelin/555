f9720b82fd25ea9481ac349eb7784a285f19be04
simhyelin <hyelisim9@gmail.com>
2026-05-12 22:45:54 +0900

    Add files via upload

--- LAB 9-1.py
+++ LAB 9-1.py
@@ -2,5 +2,7 @@ print((200).__sub__(100))
 print((200).__mul__(100))
 print((200).__truediv__(100))
 print([10, 20, 30, 40].pop())
+#다음 중 리스트 객체가 호출할 수 없는 메소드는 무엇인가?
+#2) keys(), 5) get()
 print(dir(int))
 print(dir(list))
--- LAB 9-9.py
+++ LAB 9-9.py
@@ -1,19 +1,23 @@
+import math
 class Vector:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    def magnitude(self):
+        return math.sqrt(self.x**2 + self.y**2)
+
     def __gt__(self, other):
-        return self.x > other.y
+        return self.magnitude() > other.magnitude()
 
     def __ge__(self, other):
-        return self.x >= other.y
+        return self.magnitude() >= other.magnitude()
 
     def __lt__(self, other):
-        return self.x < other.y
+        return self.magnitude() < other.magnitude()
 
     def __le__(self, other):
-        return self.x <= other.y
+        return self.magnitude() <= other.magnitude()
 
 v1 = Vector(30, 40)
 v2 = Vector(10, 20)
